82981283bffe08189cae3a7c066cbd9ed631d349
Finnvb <finnvb@gmail.com>
2022-03-21 12:18:40 +0100

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,5 @@
 let getData = fetch("https://finnvb.github.io/keep-users-in-control-reflect-for-effect/data.json"). then(response => response.json())
 .then(getData => console.log(getData));
+
+
+console.log("bhtrg");
